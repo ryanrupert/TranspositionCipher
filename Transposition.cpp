c9c05d2f84257a989c5f12bd3815a6d83639d765
Ryan Rupert <mudge25+code@gmail.com>
2017-11-07 18:16:17 -0800

    Finish Encode function

--- Transposition.cpp
+++ Transposition.cpp
@@ -1,6 +1,31 @@
 #include "Transposition.h"
 
-int Trasposition::Encode(int plain[])
+int *Trasposition::Encode(int plain[], int i_max)
 {
-	
+	//index
+	int index = 0;
+	//find the length of the array
+	int length = sizeof(plain);
+	//find the number of column's needed
+	int j_max = ceil(length / i_max);
+	//output
+	int output[length];
+	//create new 2 dem int arrays
+	table = new int [5][7];
+	for (int i = 0; i < i_max; i++) 
+	{
+		for (int j = 0; j < j_max; j++) 
+		{
+			table[i][j] = plain[index];
+			index++;
+		}
+	}
+	for (int j = 0; j < j_max; j++) 
+	{
+		for (int i = 0; i < i_max; i++) 
+		{
+			output = table[i][j];
+		}
+	}
+	return output;
 }
--- Transposition.h
+++ Transposition.h
@@ -3,9 +3,9 @@
 class Transposition
 {
 	public:
-		int Encode(int plain[]);
-		int Decode(int cipher[]);
+		int *Encode(int plain[], int i_max);
+		int *Decode(int cipher[], int i_max);
 		void clear();
 	private:
-		int *p = NULL;
+		int *table[][] = NULL;
 };
