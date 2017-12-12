335538df5ee383aff790e80bbaae35d04ce73339
Ryan Rupert <mudge25+code@gmail.com>
2017-12-12 11:42:15 -0800

    Remove result pointer in main function

--- test.cpp
+++ test.cpp
@@ -5,11 +5,10 @@ int main()
 {
 	Transposition cipher;
 	int test[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 1 };
-	int *result;
-	result = cipher.Encode(test, 10, 3);
+	cipher.Encode(test, 10, 3);
 	std::cout << "result: " << std::endl;
 	for(int i = 0; i < 10; i++)
-		std::cout << result + i << std::endl;
+		std::cout << test[i] << std::endl;
 	std::cin.get();
 	return 0;
 }
