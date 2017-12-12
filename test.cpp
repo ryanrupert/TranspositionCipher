566d44991589adbb671a06c571c722cacccb88a7
Ryan Rupert <mudge25+code@gmail.com>
2017-12-12 09:06:00 -0800

    Modify the function call

--- test.cpp
+++ test.cpp
@@ -6,7 +6,7 @@ int main()
 	Transposition cipher;
 	int test[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 1 };
 	int *result;
-	result = cipher.Encode(test, 3);
+	result = cipher.Encode(test, 10, 3);
 	std::cout << "result: " << std::endl;
 	for(int i = 0; i < 10; i++)
 		std::cout << result + i << std::endl;
